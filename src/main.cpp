36db356852853fa9a1ab8c1852b2d1a3033b0bac
BlueToadMakerr <140609260+BlueToadMakerr@users.noreply.github.com>
2025-07-31 15:35:57 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,8 @@
 #include <Geode/binding/AchievementNotifier.hpp>
 #include <Geode/binding/GJAccountManager.hpp>
 #include <Geode/utils/string.hpp>
-#include <Geode/loader/SettingNode.hpp>
 #include <Geode/ui/GeodeUI.hpp>
 #include <Geode/cocos/extensions/network/HttpClient.h>
-#include <Geode/loader/SettingNode.hpp>
 
 using namespace geode::prelude;
 
