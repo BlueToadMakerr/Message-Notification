abe5d747d4e46d0d557bd79bc5b750178b3f496a
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2025-08-07 07:20:19 -0400

    Redo it all

--- src/main.cpp
+++ src/main.cpp
@@ -1,199 +1,164 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
-#include <Geode/binding/AchievementNotifier.hpp>
-#include <Geode/binding/GJAccountManager.hpp>
-#include <Geode/utils/string.hpp>
 #include <Geode/utils/base64.hpp>
-#include <Geode/loader/Log.hpp>
-#include <Geode/cocos/extensions/network/HttpClient.h>
-
 #include <chrono>
 
 using namespace geode::prelude;
 
-class $modify(MessageChecker, MenuLayer) {
-    struct Fields {
-        inline static std::chrono::steady_clock::time_point nextCheck = std::chrono::steady_clock::now();
-        inline static bool bootChecked = false;
-    };
+struct MessageData {
 
-    static std::vector<std::string> split(const std::string& str, char delim) {
-        std::vector<std::string> elems;
-        std::stringstream ss(str);
-        std::string item;
-        while (std::getline(ss, item, delim)) {
-            elems.push_back(item);
-        }
-        return elems;
-    }
+    int messageID = -1;
+    int accountID = -1;
+    int playerID = -1;
+    std::string title;
+    std::string content;
+    std::string username;
+    std::string age;
+    bool read = false;
+    bool sender = false;
+    
+    static MessageData parseInto(const std::string& data) {
+        MessageData messageData;
+        auto split = utils::string::split(data, ":");
 
-    static std::string cleanMessage(const std::string& msg) {
-        std::vector<std::string> parts = split(msg, ':');
-        std::string cleaned;
-        for (size_t i = 0; i < parts.size(); ++i) {
-            if (parts[i] == "7" || parts[i] == "8") {
-                ++i;
+        int key = -1;
+        for (const auto& str : split) {
+            if (key == -1) {
+                key = utils::numFromString<int>(str).unwrapOr(-2);
                 continue;
             }
-            if (!cleaned.empty()) cleaned += ":";
-            cleaned += parts[i];
+            // added all the keys in case you want to expand upon what is shown.
+            switch (key) {
+                case 1:
+                    messageData.messageID = utils::numFromString<int>(str).unwrapOr(-1);
+                    break;
+                case 2:
+                    messageData.accountID = utils::numFromString<int>(str).unwrapOr(-1);
+                    break;
+                case 3:
+                    messageData.playerID = utils::numFromString<int>(str).unwrapOr(-1);
+                    break;
+                case 4:
+                    messageData.title = utils::base64::decodeString(str).unwrapOrDefault();
+                    break;
+                case 5:
+                    messageData.content = utils::base64::decodeString(str).unwrapOrDefault();
+                    break;
+                case 6:
+                    messageData.username = str;
+                    break;
+                case 7:
+                    messageData.age = str;
+                    break;
+                case 8:
+                    messageData.read = utils::numFromString<int>(str).unwrapOrDefault();
+                    break;
+                case 9:
+                    messageData.sender = utils::numFromString<int>(str).unwrapOrDefault();
+                    break;
+            }
+            key = -1;
         }
-        return cleaned;
+        return messageData;
     }
+};
 
-    void showNotification(const std::string& title) {
-        AchievementNotifier::sharedState()->notifyAchievement(
-            title.c_str(),
-            "",
-            "GJ_messageIcon_001.png",
-            false
-        );
-        log::info("notified user with title: {}", title);
+class MessageHandler : public CCNode {
+    
+    std::chrono::steady_clock::time_point m_nextCheck = std::chrono::steady_clock::now();
+    std::shared_ptr<EventListener<web::WebTask>> m_listener;
+
+    void update(float dt) {
+        int interval = Mod::get()->getSettingValue<int>("check-interval");
+        auto now = std::chrono::steady_clock::now();
+
+        if (now >= m_nextCheck) {
+            checkMessages();
+            m_nextCheck = now + std::chrono::seconds(interval);
+            log::debug("Performing message check, next check scheduled in {} seconds.", interval);
+        }
     }
 
     void checkMessages() {
-        if (Mod::get()->getSettingValue<bool>("stop-notifications")) {
-            log::info("global kill switch active - skipping check");
+        if (Mod::get()->getSettingValue<bool>("stop-notifications")
+            || (PlayLayer::get() && Mod::get()->getSettingValue<bool>("disable-while-playing"))
+            || (LevelEditorLayer::get() && Mod::get()->getSettingValue<bool>("disable-while-editing"))) {
             return;
         }
-
-        log::info("checking for new messages");
 
         auto* acc = GJAccountManager::sharedState();
         if (acc->m_accountID <= 0 || acc->m_GJP2.empty()) {
-            log::info("not logged in, skipping check");
+            log::debug("User not logged in, skipping check.");
             return;
         }
 
-        std::string postData = fmt::format("accountID={}&gjp2={}&secret=Wmfd2893gb7", acc->m_accountID, acc->m_GJP2);
-        log::info("sending HTTP request with data: {}", postData);
+        auto req = web::WebRequest();
+        req.bodyString(fmt::format("accountID={}&gjp2={}&secret=Wmfd2893gb7", acc->m_accountID, acc->m_GJP2));
+        req.userAgent("");
+        req.header("Content-Type", "application/x-www-form-urlencoded");
 
-        auto* req = new cocos2d::extension::CCHttpRequest();
-        req->setUrl("https://www.boomlings.com/database/getGJMessages20.php");
-        req->setRequestType(cocos2d::extension::CCHttpRequest::kHttpPost);
-        req->setRequestData(postData.c_str(), postData.length());
-        req->setTag("MessageCheck");
-
-        req->setResponseCallback(this, SEL_HttpResponse(&MessageChecker::onMessageResponse));
-        cocos2d::extension::CCHttpClient::getInstance()->send(req);
-        req->release();
-    }
-
-    void onMessageResponse(cocos2d::extension::CCHttpClient*, cocos2d::extension::CCHttpResponse* resp) {
-        log::info("received response from server");
-
-        if (!resp || !resp->isSucceed()) {
-            log::info("network request failed or was null");
-            return;
-        }
-
-        std::string raw(resp->getResponseData()->begin(), resp->getResponseData()->end());
-        log::info("raw server response: {}", raw);
-
-        if (raw.empty() || raw == "-1") {
-            log::info("no messages found in server response");
-            return;
-        }
-
-        auto currentRaw = split(raw, '|');
-        std::vector<std::string> currentClean;
-        for (const auto& msg : currentRaw) {
-            std::string cleaned = cleanMessage(msg);
-            currentClean.push_back(cleaned);
-            log::info("cleaned message: {}", cleaned);
-        }
-
-        std::string saved = Mod::get()->getSavedValue<std::string>("last-messages", "");
-        auto old = split(saved, '|');
-
-        std::vector<std::string> newMessages;
-        for (auto& clean : currentClean) {
-            if (std::find(old.begin(), old.end(), clean) == old.end()) {
-                newMessages.push_back(clean);
-                log::info("new message detected: {}", clean);
-            }
-        }
-
-        if (!newMessages.empty()) {
-            log::info("total new messages: {}", newMessages.size());
-
-            if (newMessages.size() == 1) {
-                auto parts = split(newMessages[0], ':');
-                if (parts.size() > 9) {
-                    std::string sender = parts[1];
-                    std::string subjectBase64 = parts[9];
-                    log::info("base64 subject: {}", subjectBase64);
-
-                    auto decoded = geode::utils::base64::decode(subjectBase64);
-                    std::string subject;
-                    if (decoded.isOk()) {
-                        auto vec = decoded.unwrap();
-                        subject = std::string(vec.begin(), vec.end());
-                        log::info("decoded subject: {}", subject);
-                    } else {
-                        subject = "<Invalid base64>";
-                        log::info("failed to decode base64 subject");
-                    }
-
-                    std::string notif = fmt::format("New Message!\nSent by: {}\n{}", sender, subject);
-                    showNotification(notif);
-                } else {
-                    showNotification("New Message!");
+        m_listener = std::make_shared<EventListener<web::WebTask>>();
+        m_listener->bind([this] (web::WebTask::Event* e) {
+            if (web::WebResponse* res = e->getValue()) {
+                if (res->ok() && res->string().isOk()) {
+                    onMessageResponse(res->string().unwrap());
                 }
-            } else {
-                showNotification(fmt::format("{} New Messages!", newMessages.size()));
+                else log::debug("Message request failed: {}", res->code());
             }
-        } else {
-            log::info("no new messages compared to saved list");
-        }
+        });
 
-        std::string save;
-        for (size_t i = 0; i < currentClean.size(); ++i) {
-            if (i > 0) save += "|";
-            save += currentClean[i];
-        }
-        Mod::get()->setSavedValue("last-messages", save);
-        log::info("saved current message state to settings");
+		auto downloadTask = req.post("https://www.boomlings.com/database/getGJMessages20.php");
+        m_listener->setFilter(downloadTask);
     }
 
-    void onScheduledTick(float) {
-        int interval = 300;
-        try {
-            interval = Mod::get()->getSettingValue<int>("check-interval");
-            log::info("using check interval from settings: {}", interval);
-        } catch (...) {
-            log::info("failed to get check-interval setting, using default 300");
+    void onMessageResponse(const std::string& data) {
+        std::vector<std::string> split = utils::string::split(data, "|");
+        // reverses the messages so they are in ascending order, allows us to count how many are new
+        std::reverse(split.begin(), split.end());
+
+        int latestID = Mod::get()->getSavedValue<int>("latest-id", 0);
+        int newMessages = 0;
+        
+        for (const auto& str : split) {
+            MessageData data = MessageData::parseInto(str);
+            if (data.messageID > latestID) {
+                latestID = data.messageID;
+                newMessages++;
+            }
         }
+        
+        // stores the message ID as they are always incremental, no need to store the whole message.
+        Mod::get()->setSavedValue("latest-id", latestID);
 
-        auto now = std::chrono::steady_clock::now();
-
-        if (now >= Fields::nextCheck) {
-            log::info("interval elapsed, performing message check");
-            checkMessages();
-            Fields::nextCheck = now + std::chrono::seconds(interval);
-            log::info("next check scheduled in {} seconds", interval);
-        } else {
-            auto remain = std::chrono::duration_cast<std::chrono::seconds>(Fields::nextCheck - now).count();
-            log::info("waiting for next check: {} seconds left", remain);
+        if (newMessages > 1) {
+            showNotification(fmt::format("{} New Messages!", newMessages), "Check them out!");
+        }
+        else if (newMessages == 1) {
+            MessageData data = MessageData::parseInto(split[split.size()-1]);
+            showNotification(fmt::format("New Message from: {}", data.username), data.title);
         }
     }
 
-    bool init() {
-        if (!MenuLayer::init()) return false;
-
-        log::info("MenuLayer init");
-
-        if (!Fields::bootChecked) {
-            Fields::bootChecked = true;
-            log::info("boot sequence starting");
-
-            this->schedule(schedule_selector(MessageChecker::onScheduledTick), 1.0f);
-            onScheduledTick(0);
-        } else {
-            log::info("boot already done, skipping init check and re-running the schedule");
-            this->schedule(schedule_selector(MessageChecker::onScheduledTick), 1.0f);
-        }
-
-        return true;
+    void showNotification(const std::string& title, const std::string& content) {
+        // use quest bool so it doesn't act like an actual achievement alert saying an icon was unlocked.
+        AchievementNotifier::sharedState()->notifyAchievement(
+            title.c_str(),
+            content.c_str(),
+            "accountBtn_messages_001.png",
+            true
+        );
     }
+
+public:
+    static MessageHandler* create() {
+		auto ret = new MessageHandler();
+		ret->autorelease();
+		return ret;
+	}
 };
+
+$execute {
+	Loader::get()->queueInMainThread([]{
+		CCScheduler::get()->scheduleUpdateForTarget(MessageHandler::create(), INT_MAX, false);
+	});
+}
